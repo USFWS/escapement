ea321a0dc4a8ffb2641c463641b314f856eae615
Cobb <mccrea_cobb@fws.gov>
2020-11-12 10:46:12 -0900

    Updates header.tex and before_body.tex. Updates 2018 data.

--- R/import_format.R
+++ R/import_format.R
@@ -13,20 +13,19 @@
 #'
 #' @examples
 #' #' \dontrun{
-#' import_format(input = "./data/raw/hourraw.csv", saveit = TRUE, output = "./data/derived/dat.Rdata")
+#' import_format(input = "./data/raw/salmon_counts.csv", saveit = TRUE, output = "./data/derived/dat.Rdata")
 #' }
 import_format <- function(input,
                           saveit = FALSE,
                           output){
-
   dat <- read.csv(input)  # Read in the raw data (csv)
 
   dat$date <- as.POSIXct(as.character(dat$date),
                          format="%m/%d/%Y %H:%M")  # Change date to POSIXct
   dat$date <- round(dat$date, units = "hours") # round to the nearest hour
 
-  dat$photo <- as.numeric(dat$photo)
-  dat$video <- as.numeric(dat$video)
+  dat$photo <- suppressWarnings(as.numeric(dat$photo))
+  dat$video <- suppressWarnings(as.numeric(dat$video))
   dat$julian_day <- as.numeric(format(as.Date(dat$date),
                                   "%j"))  # Add a Julian day column
   dat$year <- lubridate::year(dat$date)
