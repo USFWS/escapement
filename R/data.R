14882d225c3c293a71f1a2fa90923550d30e9f78
Cobb <mccrea_cobb@fws.gov>
2020-12-03 16:57:54 -0900

    Adds data/salmon_counts.rda and documentation of these data. Updates intro vignette.

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Video and photo counts of sockeye salmon at Akalura River, Kodiak Island, Alaska
+#'
+#' @source Kodiak National Wildlife Refuge, Kodiak, Alaska
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{date}{A POSIXlt date}
+#'  \item{photo}{Number of salmon counted using photos (numeric)}
+#'  \item{video}{Number of salmon counted using videos (numeric)}
+#'  \item{julian_day}{The julian day (numeric)}
+#'  \item{year}{The year (numeric)}
+#' }
+#' @examples
+#' \dontrun{
+#'  salmon_counts
+#' }
+"salmon_counts"
